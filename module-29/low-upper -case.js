53516644d996e194f6160099561213e397473d35
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-02-14 14:32:53 +0600

    add low-upper-case js

---
+++ module-29/low-upper -case.js
@@ -0,0 +1,31 @@
+const inputName = 'MD shihab UddIn is a student Of tHe university if Rajshahi.';
+// transform to lowercase.
+
+const nameLowerCase = inputName.toLowerCase();
+console.log(nameLowerCase);
+
+// transfrom to uppercase
+
+const nameUppercase = inputName.toUpperCase();
+console.log(nameUppercase);
+
+// add condition tow name 
+
+const name1 = "beAuTifUL";
+const name2 = 'BeaUtIFul';
+
+// for lowercase
+
+if (name1.toLowerCase == name2.toLowerCase) {
+    console.log('Yes you are right.')
+} else {
+    console.log('Sorry you are wrong.')
+}
+
+// for uppercase
+
+if (name1.toUpperCase == name2.toUpperCase) {
+    console.log('Yes you are right.')
+} else {
+    console.log('Sorry you are wrong.')
+}
